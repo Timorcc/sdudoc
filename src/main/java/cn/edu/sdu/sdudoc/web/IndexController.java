bbddc5e5c4e25ba383ad3f8c7ecf4b272ae369d2
Timorcc <1148137751@qq.com>
2019-11-08 11:24:19 +0800

    添加日志打印

--- src/main/java/cn/edu/sdu/sdudoc/web/IndexController.java
+++ src/main/java/cn/edu/sdu/sdudoc/web/IndexController.java
@@ -2,18 +2,21 @@ package cn.edu.sdu.sdudoc.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
 
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name) {
 
-    @GetMapping("/")
-    public String index(){
+        System.out.println("------index------");
         return "index";
     }
+
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
-}
+    }
 }
