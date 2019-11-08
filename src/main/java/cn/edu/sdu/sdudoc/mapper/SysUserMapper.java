31d8aff3ce4e1fc48618f17f79e365e1ee478b5a
Jhno22 <15563856357@163.com>
2019-11-08 09:26:02 +0800

    添加的mybatis逆向生成工具

---
+++ src/main/java/cn/edu/sdu/sdudoc/mapper/SysUserMapper.java
@@ -0,0 +1,17 @@
+package cn.edu.sdu.sdudoc.mapper;
+
+import cn.edu.sdu.sdudoc.pojo.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
---
+++ src/main/java/cn/edu/sdu/sdudoc/pojo/SysUser.java
@@ -0,0 +1,47 @@
+package cn.edu.sdu.sdudoc.pojo;
+
+import java.io.Serializable;
+
+public class SysUser implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private String mobile;
+
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
